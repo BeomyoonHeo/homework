ba3e5cb808a60ed753524af41859213d9db03faf
BeomyoonHeo <gqd0512@naver.com>
2022-09-05 12:10:53 +0900

    View 를 DTO로 변경 - 코드리펙토링

--- src/main/java/site/metacoding/red/domain/boards/BoardsDao.java
+++ src/main/java/site/metacoding/red/domain/boards/BoardsDao.java
@@ -2,14 +2,14 @@ package site.metacoding.red.domain.boards;
 
 import java.util.List;
 
-import site.metacoding.red.domain.boards.mapper.MainView;
 import site.metacoding.red.domain.boards.mapper.ShowDetail;
+import site.metacoding.red.web.dto.response.boards.MainDto;
 
 
 public interface BoardsDao {
 	public void insert(Boards boards); // DTO 생각해보기
 	public ShowDetail findById(Integer id);
-	public List<MainView> findAll();
+	public List<MainDto> findAll();
 	public void update(Boards boards); // DTO 생각해보기
 	public void delete(Integer id);
 }
